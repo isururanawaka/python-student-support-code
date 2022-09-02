7e2629129b7a804f614c0ec2c5196cd5ee804e5f
Isuru Ranawaka <irjanith@gmail.com>
2022-09-02 10:53:32 -0400

    implement complex operand removal

--- compiler.py
+++ compiler.py
@@ -1,4 +1,5 @@
 import ast
+import utils
 from ast import *
 from utils import *
 from x86_ast import *
@@ -15,19 +16,106 @@ class Compiler:
     # Remove Complex Operands
     ############################################################################
 
+    def is_atm(self, atm):
+        match atm:
+            case Constant(i):
+                return True
+            case Name(v):
+                return True
+            case _:
+                return isinstance(atm, str)
+
     def rco_exp(self, e: expr, need_atomic: bool) -> Tuple[expr, Temporaries]:
-        # YOUR CODE HERE
-        pass
+        # print("calling exp")
+        if not need_atomic:
+            return [e, []]
+        else:
+            match e:
+                case Call(Name('input_int'), []):
+                    return [Call(Name('input_int'), []), []]
+                case UnaryOp(unartop, atm):
+                    if self.is_atm(atm):
+                        return [UnaryOp(unartop, atm), []]
+                    else:
+                        (re_exp, tmps) = self.rco_exp(atm, True)
+                        temps = []
+                        temps.extend(tmps)
+                        temps.append(Name(utils.generate_name("temp")), temps)
+                        return [UnaryOp(unartop), temps]
+                case BinOp(left, binaryop, right):
+                    temps = []
+                    left_exp = left
+                    right_exp = right
+                    if not self.is_atm(left):
+                        (left_exp, left_temps) = self.rco_exp(left, True)
+                        temps.extend(left_temps)
+                        left_name = utils.generate_name("temp")
+                        temps.append((Name(left_name), left_exp))
+                        left_exp = Name(left_name)
+
+                    if not self.is_atm(right):
+                        (right_exp, right_temps) = self.rco_exp(right, True)
+                        temps.extend(right_temps)
+                        right_name = utils.generate_name("temp")
+                        temps.append((Name(right_name), right_exp))
+                        right_exp = Name(right_name)
+                    return [BinOp(left_exp, binaryop, right_exp), temps]
+
+    pass
 
     def rco_stmt(self, s: stmt) -> List[stmt]:
-        # YOUR CODE HERE
+        res = []
+        match s:
+            case Expr(Call(Name('print'), [atm])):
+                for at in [atm]:
+                    if self.is_atm(atm):
+                        (exp_atm, temps) = self.rco_exp(at, False)
+                        res.append(Expr(Call(Name('print'), [exp_atm])))
+                    else:
+                        (exp_atm, temps) = self.rco_exp(at, True)
+                        for tmp in temps:
+                            res.append(Assign([tmp[0]], tmp[1]))
+                        exp = Expr(Call(Name('print'), [exp_atm]))
+                        res.append(exp)
+                return res
+
+            case Expr(exp):
+                if self.is_atm(exp):
+                    (exp_atm, temps) = self.rco_exp(exp, False)
+                    res.append(Expr(exp_atm))
+                else:
+                    (exp_atm, temps) = self.rco_exp(exp, True)
+                    for tmp in temps:
+                        res.append(Assign([tmp[0]], tmp[1]))
+                res.append(Expr(exp_atm))
+                return res
+
+            case Assign([Name(var)], exp):
+                if self.is_atm(exp):
+                    (exp_atm, temps) = self.rco_exp(exp, False)
+                    res.append(Assign([Name(var)], exp_atm))
+                else:
+                    (exp_atm, temps) = self.rco_exp(exp, True)
+                    for tmp in temps:
+                        res.append(Assign([tmp[0]], tmp[1]))
+                    res.append(Assign([Name(var)], exp_atm))
+                return res
+            case _:
+                print("Unknown type")
         pass
-        
 
     def remove_complex_operands(self, p: Module) -> Module:
-        # YOUR CODE HERE
+        match p:
+            case Module(stmt):
+                st_list = []
+                for st in stmt:
+                    re_list = self.rco_stmt(st)
+                    st_list.extend(re_list)
+                return Module(st_list)
+            case _:
+                print("Unknown type")
+
         pass
-        
 
     ############################################################################
     # Select Instructions
@@ -35,15 +123,16 @@ class Compiler:
 
     def select_arg(self, e: expr) -> arg:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def select_stmt(self, s: stmt) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
-    # def select_instructions(self, p: Module) -> X86Program:
+        # def select_instructions(self, p: Module) -> X86Program:
+
     #     # YOUR CODE HERE
-    #     pass        
+    #     pass
 
     ############################################################################
     # Assign Homes
@@ -51,21 +140,22 @@ class Compiler:
 
     def assign_homes_arg(self, a: arg, home: Dict[Variable, arg]) -> arg:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes_instr(self, i: instr,
                            home: Dict[location, arg]) -> instr:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes_instrs(self, ss: List[instr],
                             home: Dict[location, arg]) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
-    # def assign_homes(self, p: X86Program) -> X86Program:
+        # def assign_homes(self, p: X86Program) -> X86Program:
+
     #     # YOUR CODE HERE
-    #     pass        
+    #     pass
 
     ############################################################################
     # Patch Instructions
@@ -73,15 +163,15 @@ class Compiler:
 
     def patch_instr(self, i: instr) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def patch_instrs(self, ss: List[instr]) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
-    # def patch_instructions(self, p: X86Program) -> X86Program:
+        # def patch_instructions(self, p: X86Program) -> X86Program:
     #     # YOUR CODE HERE
-    #     pass        
+    #     pass
 
     ############################################################################
     # Prelude & Conclusion
@@ -89,5 +179,4 @@ class Compiler:
 
     # def prelude_and_conclusion(self, p: X86Program) -> X86Program:
     #     # YOUR CODE HERE
-    #     pass        
-
+    #     pass
